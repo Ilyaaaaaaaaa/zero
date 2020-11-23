7dd5e40fcbd98666c31dcc522e79e12364cf1105
Ilyaaaaaaaaa <67660042+Ilyaaaaaaaaa@users.noreply.github.com>
2020-11-23 15:44:14 +0300

    zero

--- zero.java
+++ zero.java
@@ -7,16 +7,19 @@ import java.util.Scanner;
         public class zero {
             public static void main(String[] args) {
                 int [] mas = new int[8];
-
+                int length = mas.length;
                 for (int i = 0; i < mas.length; i++) {
-                    mas[i] = (int)(Math.random()*10)+1;
+                    mas[i] = ((int)(Math.random()*10) -10);
+
                 }
                 System.out.println(Arrays.toString(mas));
                 for (int i = 0; i < mas.length; i++) {
-                    if(i%2 == 1){
+                    if(mas[i]%2 != 0){
                         mas[i] = 0;
                     }
                 }
+
+
                 System.out.println(Arrays.toString(mas));
             }
         }
